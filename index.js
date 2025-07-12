bf57068f4e7520bdef208171bd4fd4b673ea859e
varshikakarthikeyan <varshikakarthikeyan584@gmail.com>
2025-07-12 10:46:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//task1
 var number=13;
 var name='js';
 var iscloudy=true;
@@ -5,3 +6,16 @@ console.log(number+ " "+name+ " " +iscloudy);
 document.write(number);
 document.write(name);
 document.write(iscloudy);
+
+//task2
+console.log(typeof(num));
+console.log(typeof(name));
+console.log(typeof(iscloudy));
+
+//task3
+var number1=parseInt(prompt("Enter the first value"));
+var number2=parseInt(prompt("Enter the second value"));
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
